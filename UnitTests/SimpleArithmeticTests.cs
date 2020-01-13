9c411c7a47a4c7d47b2e65246621ececcfcdcf34
popliuc <danut_00@yahoo.com>
2020-01-13 21:27:09 +0200

    Add files via upload

---
+++ UnitTests/SimpleArithmeticTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SimpleArithmeticTests
+    {
+        [TestMethod]
+        public void TestAddition()
+        {
+            var simpleArithmetic = new SimpleArithmetic();
+
+            Assert.IsTrue(simpleArithmetic.Addition(1, 1) == 2);
+        }
+
+        [TestMethod]
+        public void TestSubtraction()
+        {
+            var simpleArithmetic = new SimpleArithmetic();
+
+            Assert.IsTrue(simpleArithmetic.Subtraction(1, 1) == 0);
+        }
+
+
+    }
+}
---
+++ UnitTests/SimpleArithmeticTests2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SimpleArithmeticTests2
+    {
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            var simpleArithmetic = new SimpleArithmetic();
+
+            Assert.IsTrue(simpleArithmetic.Multiplication(2, 10) == 20);
+        }
+
+        [TestMethod]
+        public void TestDivision()
+        {
+            var simpleArithmetic = new SimpleArithmetic();
+
+            Assert.IsTrue(simpleArithmetic.Division(8, 2) == 4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestDivisionByZero()
+        {
+            var simpleArithmetic = new SimpleArithmetic();
+
+            simpleArithmetic.Division(2, 0);
+        }
+    }
+}
